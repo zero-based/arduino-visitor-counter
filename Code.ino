5a01006115bf8d6fdf24847d45e68376b7cef2e2
Muntadhar Haydar <sparkist97@outlook.com>
2019-09-08 20:36:31 +0300

    enhance the code
    getting rid of "Delay" will improve the performance clearly since it halts everything and should anyone enters or leaves during the two seconds of delaly they won't be counted
    
    also getting rid of the "if" statements
    
    * note: I didn't test it but it should work

--- Code.ino
+++ Code.ino
@@ -17,6 +17,8 @@ int led2Pin = 13;
 //INITIALIZE COUNTER :
 int count = 0;
 
+unsigned long millis = 0;
+
 void setup(){
     //LCD SETTINGS:
     lcd.begin(16,2);
@@ -41,36 +43,18 @@ void loop(){
     if(digitalRead(out)){
         count--;
     }
-
-    //LCD DISPLAY :
-    lcd.clear();
-    lcd.print("Person In Room:");
-    lcd.setCursor(0,1);
-    lcd.print(count);
-    delay (1000);
     
-    //LED LIGHT OFF/ON:
-    if(count <= 0){
+    if (millis() - millis >= 1000) {
+        // if it has been a second more, update the display
         lcd.clear();
-        digitalWrite(led001Pin, LOW);
-        digitalWrite(led01Pin , LOW);
-        digitalWrite(led1Pin  , LOW);
-        lcd.clear();
-        lcd.print("Nobody In Room");
-        lcd.setCursor(0,1);
-        lcd.print("Light Is Off");
-        delay(1000);
+        lcd.print(count > 0 ? "People here: " : "Nobody here");
+        lcd.setCursor(0, 1);
+        lcd.print(count > 0 ? str(count) : "Lights are off");
+        millis = millis();
     }
-    if(count >= 1 && count <= 5){
-        digitalWrite(led001Pin, HIGH);
-        digitalWrite(led01Pin , HIGH);
-        digitalWrite(led1Pin  , HIGH);
-        digitalWrite(led2Pin  , LOW);
-    }
-    if(count > 5 && count <= 10){
-        digitalWrite(led001Pin, HIGH);
-        digitalWrite(led01Pin , HIGH);
-        digitalWrite(led1Pin  , HIGH);
-        digitalWrite(led2Pin  , HIGH);
-    }   
+    
+    digitalWrite(led001Pin, count > 0);
+    digitalWrite(led01Pin, count > 0);
+    digitalWrite(led1Pin, count > 0);
+    digitalWrite(led2Pin, count > 5);  
 }
